d01774d15a3d6a2a70983ee68bebcd27205b1a13
Eduh1986 <152194713+Eduh1986@users.noreply.github.com>
2024-01-17 20:14:14 -0300

    modificaciones para el trabaja numero 3

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+const resultGrid = document.getElementById('result-grid');
+
+export const app = () => {
+       // Ejecutamos actividad iniciales
+       console.log("Ejecutando aplicación");
+       }
