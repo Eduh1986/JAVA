2f686a129a842ffbe51a069e567f28dff1021934
Eduh1986 <edgardo_corbalan@hotmail.com>
2024-02-03 09:58:59 -0300

    mejoras

--- src/api/comprasUsuarios.api.js
+++ src/api/comprasUsuarios.api.js
@@ -1,6 +1,6 @@
 // Obtener  las compras del usuario por el idUsuario
-const obtenerComprasUsuario = async (idUsuario) => {
-    const resp = await fetch(`http://localhost:3000/comprasUsuarios/${idUsuario}`);
+const obtenerComprasUsuario = async () => {
+    const resp = await fetch("http://localhost:3000/comprasUsuarios");
     const data = await resp.json();
     console.log(data);
     return data;    
