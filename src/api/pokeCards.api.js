a6cb0fb25a1a0029c43298c4dfb04ec970795d4c
Eduh1986 <edgardo_corbalan@hotmail.com>
2024-02-04 17:31:28 -0300

    mejora

---
+++ src/api/pokeCards.api.js
@@ -0,0 +1,10 @@
+const obtenerPokemonPorNombre = async (nombre) => {
+    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`);
+    const data = await resp.json();
+    console.log(data);
+    return data; 
+}
+
+export default {
+    obtenerPokemonPorNombre
+}
