d094752a690611f945ba3b771b8bae5d09e59da5
Eduh1986 <edgardo_corbalan@hotmail.com>
2024-02-04 23:07:39 -0300

    mejora

--- src/api/pokeCards.api.js
+++ src/api/pokeCards.api.js
@@ -19,16 +19,23 @@ let resultadoCard = document.getElementById("resultadoCard");
         <main class="flex">
             <article class="card">
                 <div class="card-body">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${personajes.id}.svg" alt="imagen de vitoko" class="card-body-img"style="width: 150px;">
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${personajes.id}.svg" alt="imagen de vitoko" class="card-body-img"style="width: 100px;">
                     <h1 class="card-body-title">
+                    ${personajes.name}
                     </h1>
                 </div>
                 <div class="card-footer">
                     <div class="card-footer-social">
+                    <h3>${personajes.stats[1].base_stat}</h3>
+                    <p>Ataque</p>
                     </div>
                     <div class="card-footer-social">
+                    <h3>${personajes.stats[2].base_stat}</h3>
+                    <p>Especial</p>
                     </div>
                     <div class="card-footer-social">
+                    <h3>${personajes.stats[3].base_stat}</h3>
+                    <p>Defensa</p>
                     </div>
                 </div>
             </article>
